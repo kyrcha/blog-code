6d3f486d37bd27da05956756f5b029beb94e1679
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2019-11-28 10:53:14 +0200

    Add windowed Gaussian vis

--- nab-results-vis.R
+++ nab-results-vis.R
@@ -1,7 +1,15 @@
 # Visualizing anomaly detection output
 
-resultsFile <- "../NAB/results/chebyshev/realAWSCloudwatch/chebyshev_ec2_cpu_utilization_24ae8d.csv"
+# Chebushev
+#resultsFile <- "../NAB/results/chebyshev/realAWSCloudwatch/chebyshev_ec2_cpu_utilization_24ae8d.csv"
+#threshold = 0
+
+# Windowed Gaussian
+resultsFile <- "../NAB/results/windowedGaussian/realAWSCloudwatch/windowedGaussian_ec2_cpu_utilization_24ae8d.csv"
+threshold = 0.9999999999997738
+
+# Read and Plot
 data <- read.csv(resultsFile)
 plot(data$value, type="l", main = "EC2 CPU Utilization", ylab = "CPU Utilization")
 points(which(data$label == 1), data$value[data$label == 1], col="red")
-points(which(data$anomaly_score > 0.0), data$value[data$anomaly_score > 0.0], col="blue", pch="x")
+points(which(data$anomaly_score > threshold), data$value[data$anomaly_score > threshold], col="blue", pch="x")
