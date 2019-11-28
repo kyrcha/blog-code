6ee03bc3bf34d365ecae5c692ed807fd3028777a
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2019-11-28 10:44:02 +0200

    Small improvement

--- nab-results-vis.R
+++ nab-results-vis.R
@@ -1,6 +1,7 @@
 # Visualizing anomaly detection output
 
-resultsFile <- "../NAB/results/chebyshev/realAWSCloudwatch/chebyshev_ec2_cpu_utilization_5f5533.csv"
+resultsFile <- "../NAB/results/chebyshev/realAWSCloudwatch/chebyshev_ec2_cpu_utilization_24ae8d.csv"
 data <- read.csv(resultsFile)
 plot(data$value, type="l", main = "EC2 CPU Utilization", ylab = "CPU Utilization")
 points(which(data$label == 1), data$value[data$label == 1], col="red")
+points(which(data$anomaly_score > 0.0), data$value[data$anomaly_score > 0.0], col="blue", pch="x")
