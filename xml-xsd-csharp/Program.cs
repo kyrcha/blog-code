f68bdaaf374c6620776788f5679738f04b536403
Kyriakos Chatzidimitriou <chatzidimitriou.k@sga.gr>
2020-04-24 13:06:40 +0300

    Initial commit xml-xsd

---
+++ xml-xsd-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xml_xsd_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
