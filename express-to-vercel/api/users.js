c89b1db211b56ec8b6e881136398e0a217dd4ace
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2020-04-29 15:55:17 +0300

    Moved express-to-vercel to another repo

--- express-to-vercel/api/users.js
+++
@@ -1,8 +0,0 @@
-const { connect } = require('./_mongoose');
-const User = require('./_models/user');
-
-module.exports = async (req, res) => {
-  await connect();
-  const users = await User.find({});
-  return res.json(users);
-};
